9ce5e06e0bd3b4f8ade2ddf8b3f4c4139fa3449b
Ryosuke-Kawashima-Career <ryosuke.kawashima.career@gmail.com>
2026-05-14 22:37:03 +0530

    Minimum strategy for producing signals is added.

--- strategy.py
+++ strategy.py
@@ -3,6 +3,29 @@ import pandas as pd
 import math
 from mlbacktester import AssetInfo, BaseStrategy, Order
 
+class BaseStrategy(metaclass=ABCMeta):
+    def __init__(self, cfg: dict) -> None:
+        self.cfg = cf
+    def preprocess(self, raw_df: pd.DataFrame) -> pd.DataFrame:
+        return raw_df
+
+    @abstractmethod
+    def get_signal(self, preprocessed_df: pd.DataFrame, model: Any) -> pd.DataFrame:
+        pass
+
+    def get_model(self, train_df: pd.DataFrame) -> Any:
+        pass
+
+    @abstractmethod
+    def get_orders(
+        self,
+        latest_timestamp: pd.Timestamp,
+        latest_bar: pd.Series,
+        latest_signal: pd.Series,
+        asset_info: AssetInfo,
+    ) -> list[Order]:
+        pass
+
 class Strategy(BaseStrategy):
     def __init__(self, cfg) -> None:
         super().__init__(cfg)
@@ -218,4 +241,146 @@ class Strategy(BaseStrategy):
                                       price=None,
                                       symbol=symbol))
 
+        return order_lst
+
+class MinimumStrategy(BaseStrategy):
+    def __init__(self, cfg: dict) -> None:
+        super().__init__(cfg)
+        self.symbols = cfg["backtester_config"]["symbol"]
+
+    def preprocess(self, df):
+        """
+        preprocessing function for minimum strategy
+
+        Parameters
+        ==========
+        df: pandas.DataFrame
+            dataframe contains ohlcv data
+
+        Returns
+        ==========
+        preprocessed_df: pandas.DataFrame
+            preprocessed dataframe
+        """
+        ### preprocessing for get_orders
+        # calculate annual risk from past 30 days price data
+        span = 24 * 7 * 4  # 4 weeks
+        df['volatility'] = np.nan
+
+        # calculate volatility for each symbol
+        for symbol in df.index.get_level_values('symbol').unique():
+            # get data for each symbol
+            symbol_df = df.xs(symbol, level='symbol')
+
+            # calculate log return
+            log_return = np.log(symbol_df['close']).diff()
+
+            # calculate rolling std
+            rolling_std = log_return.rolling(window=span).std()
+
+            # calculate annualized volatility
+            annualized_vola_symbol = rolling_std * np.sqrt(365.25 * 24)
+            annualized_vola_symbol.fillna(1, inplace=True)
+
+            # store result to original dataframe
+            df.loc[(symbol_df.index, symbol), 'volatility'] = annualized_vola_symbol.values
+        ### preprocessing for get_orders end
+
+        preprocessed_df = df.copy()
+        # add preprocessing
+
+        return preprocessed_df
+
+    def get_model(self, train_df: pd.DataFrame) -> Any:
+        return None
+
+    def get_signal(self, preprocessed_df: pd.DataFrame, model: Any) -> pd.DataFrame:
+        return preprocessed_df.copy().assign(signal=1.0)
+
+    def get_orders(self, latest_timestamp, latest_bar, latest_signal, asset_info):
+        """
+        create orders based on the order time, the status of positions at that time, and the signal obtained from OHLCV
+
+        Parameters
+        ==========
+        latest_timestamp : pandas.Timestamp
+            current time (latest_timestamp)
+        latest_bar : pandas.DataFrame
+            OHLCV data (multi index of symbol and time)
+        latest_signal : pandas.DataFrame
+            latest signal for each symbol (multi index of symbol and time)
+        asset_info : AssetInfo
+            asset information (position, margin, etc.)
+
+        Returns
+        =======
+        list[Order]
+            list of orders for latest_timestamp
+        """
+
+        order_lst = []
+        d = 0.35  # Discretization level
+
+        # Calculate size ratio dynamically based on price ratios at the current time
+        latest_prices = {
+            symbol: latest_bar.loc[(slice(None), symbol), :].iloc[0]["close"]
+            for symbol in self.cfg["backtester_config"]["symbol"]
+        }
+        inv_prices = {symbol: 1 / price for symbol, price in latest_prices.items()}
+        total_inv_price = sum(inv_prices.values())
+        size_ratio = {
+            symbol: inv_price / total_inv_price
+            for symbol, inv_price in inv_prices.items()
+        }
+
+        # Create orders for each symbol
+        for symbol in self.cfg["backtester_config"]["symbol"]:
+            latest_signal_symbol = latest_signal.loc[(slice(None), symbol), :].iloc[0]
+            latest_bar_symbol = latest_bar.loc[(slice(None), symbol), :].iloc[0]
+
+            pos_size = asset_info.signed_pos_sizes[symbol]
+
+            # Get signal value and discretize
+            signal_value = latest_signal_symbol["signal"]
+            if pd.isna(signal_value):
+                signal_value = 0.0
+
+            target_position_size = (
+                math.floor(signal_value / d) * 0.5
+                if signal_value > 0
+                else math.ceil(signal_value / d) * 0.5
+            )
+
+            # Set annualized risk target
+            match target_position_size:
+                case 1:
+                    annualized_risk_target = 0.5
+                case 0.5:
+                    annualized_risk_target = 0.25
+                case -0.5:
+                    annualized_risk_target = -0.25
+                case -1:
+                    annualized_risk_target = -0.5
+                case _:
+                    annualized_risk_target = 0
+
+            # Get relevant volatility
+            relevant_vola = latest_signal_symbol["volatility"]
+
+            # Calculate target size considering dynamic size ratio
+            target_size = (annualized_risk_target / relevant_vola) * size_ratio[symbol]
+            order_size = target_size - pos_size
+            side = "BUY" if order_size > 0 else "SELL"
+
+            # Add order only if the minimum trading unit is met
+            if abs(order_size) >= self.cfg["exchange_config"][symbol]["min_lot"]:
+                order_lst.append(
+                    Order(
+                        type="MARKET",
+                        side=side,
+                        size=abs(order_size),
+                        price=None,
+                        symbol=symbol,
+                    )
+                )
         return order_lst
